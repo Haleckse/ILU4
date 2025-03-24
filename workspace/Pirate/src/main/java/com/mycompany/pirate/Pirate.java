81adbc347e1cbf23ab5bb47c650791efd074535e
Alex Tsan <tsl4297a@ensfsi.onmicrosoft.com>
2025-03-24 11:43:55 +0100

    debut projet pirates

---
+++ workspace/Pirate/src/main/java/com/mycompany/pirate/Pirate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pirate;
+
+/**
+ *
+ * @author tsl4297a
+ */
+public class Pirate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
